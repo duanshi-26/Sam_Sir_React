27c0f9b907bcf397b042450da8f5e85e46a8fcbc
duanshi-26 <duanshichawla2612@gmail.com>
2025-04-14 14:50:51 +0530

    Creating elements using JS and React CDNs

---
+++ Session_57/UsingJS/app.js
@@ -0,0 +1,9 @@
+// 1. normal javascript se render karna 
+let rootEl = document.getElementById('root');
+// create element to be added
+let p1 = document.createElement('p');
+// add content to element using innerHTML
+p1.innerHTML="Writing element using JS"
+// append the element to root
+rootEl.append(p1);
+
---
+++ Session_57/UsingReact/app.js
@@ -0,0 +1,15 @@
+// fetch the root div
+
+let root = document.getElementById('root');
+
+// create element using React ka "createElement method" jiske 3 arguemnts hai 
+// 1. type
+// 2. props ya attributes
+// 3. children ya content
+
+let ele = React.createElement('p', {id:"para", className: "graph"}, "Writing element using React")
+
+// ab elemt ko root par render kardo using ReactDOM
+
+let r = ReactDOM.createRoot(root);
+r.render(ele);
